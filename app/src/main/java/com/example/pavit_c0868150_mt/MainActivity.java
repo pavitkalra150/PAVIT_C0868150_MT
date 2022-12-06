67cd1d191c29cc02ca17d27be619a9e6cfe10794
pavitkalra150 <c0868150@mylambton.ca>
2022-12-05 19:03:58 -0500

    Initial setup

---
+++ app/src/main/java/com/example/pavit_c0868150_mt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pavit_c0868150_mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
